0a923d378aaf35f72eb865fe3902b1175859ab23
satamame <satax@nifty.com>
2021-05-04 00:02:08 +0900

    Load tsv to show list with temlate.

--- quiz_web.py
+++ quiz_web.py
@@ -1,11 +1,20 @@
-from flask import Flask
+import csv
+from collections import namedtuple
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+Quizset = namedtuple('Quizset', 'filename title')
 
 
 @app.route("/")
 def index():
-    return "This is index page."
+    with open('quiz/index.txt', newline='', encoding='utf-8-sig') as f:
+        rows = csv.reader(f, delimiter='\t')
+        sets = [Quizset(*row) for row in rows]
+
+    return render_template('index.html', title='Quiz Web', sets=sets)
 
 
 if __name__ == "__main__":
