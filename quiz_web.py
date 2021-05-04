227f481f627ebea465f8101095e2a840bc233a98
satamame <satax@nifty.com>
2021-05-04 18:58:27 +0900

    Quiz set page linked from Index page.

--- quiz_web.py
+++ quiz_web.py
@@ -6,17 +6,47 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-Quizset = namedtuple('Quizset', 'filename title')
+QuizSet = namedtuple('QuizSet', 'filename title description')
+QuizData = namedtuple('QuizData', 'question comment choices')
 quiz_dir = Path(__file__).parent.resolve() / 'quiz'
 
 
-@app.route("/")
-def index():
-    with open(quiz_dir / 'index.txt', newline='', encoding='utf-8-sig') as f:
+def get_qsets():
+    """クイズ集のリストを取得する
+    """
+    with open(quiz_dir / 'index.tsv', newline='', encoding='utf-8-sig') as f:
         rows = csv.reader(f, delimiter='\t')
-        sets = [Quizset(*row) for row in rows]
+        qsets = [QuizSet(*row) for row in rows]
+    return qsets
 
-    return render_template('index.html', title='Quiz Web', sets=sets)
+
+def get_qset_data(filename):
+    """クイズ集のデータを取得する
+    """
+    with open(quiz_dir / filename, newline='', encoding='utf-8-sig') as f:
+        rows = csv.reader(f, delimiter='\t')
+        qset_data = [QuizData(row[0], row[1], row[2:]) for row in rows]
+    return qset_data
+
+
+@app.route('/')
+def index():
+    """インデックスページを返す
+    """
+    title = 'Quiz Web - Index'
+    return render_template('index.html', title=title, qsets=get_qsets())
+
+
+@app.route('/qset/<int:qset_idx>')
+def quiz_set(qset_idx=0):
+    """クイズ集のクイズ一覧を返す
+    """
+    qset = get_qsets()[qset_idx]
+    qset_data = get_qset_data(qset.filename)
+    title = f'Quiz Web - {qset.title}'
+    return render_template(
+        'quiz_set.html', title=title,
+        qset_idx=qset_idx, qset=qset, qset_data=qset_data)
 
 
 if __name__ == "__main__":
